470f9b19f6378d7462287b4a5273fd14dabb94de
Atharva804 <74488182+Atharva804@users.noreply.github.com>
2023-09-27 21:17:36 +0530

    Added start gamr and new card function

--- index.js
+++ index.js
@@ -1,22 +1,35 @@
 let firstCard = 6
 let secondCard = 9
+let cards = [firstCard, secondCard]
 let sum = firstCard + secondCard
 let hasBlackJack = false
 let isAlive = true
+let message = ""
+let messageEl = document.getElementById("msg-el")
+let sumEl = document.querySelector("#sum-el")
+let cardsEl = document.querySelector("#cards-el")
 
-if (sum <= 20) {
-    console.log("Do you want to draw a new card?")
-} else if (sum === 21) {
-    console.log("Yay! You've got blackjack!")
-    hasBlackJack = true
-} else {
-    console.log("You're out of the game!")
-    isAlive = false
+function startGame () {
+    renderGame()
 }
 
-let age = 22
-if (age <= 20) {
-    console.log("You can not enter the club!")
-} else {
-    console.log("Welcome!")
+function renderGame() {
+    sumEl.textContent = "Sum: " + sum
+    cardsEl.textContent = "Cards: " + cards[0] + " " + cards[1]
+    if (sum <= 20) {
+        message = "Do you want to draw a new card?"
+    } else if (sum === 21) {
+        message = "You've got blackjack!"
+        hasBlackJack = true
+    } else {
+        message = "You're out of the game!"
+        isAlive = false
+    }
+    messageEl.textContent = message
+}
+
+function newCard() {
+    let card = 6
+    sum += card
+    renderGame()
 }
